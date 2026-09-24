75a9e7ecdef4c8753d6ac7b0f12f0fa11c740427
Joaquincamps <joaquin.campsmartinez@educa.madrid.org>
2026-09-24 14:35:37 +0200

    First commit

---
+++ src/main/java/com/example/RadioTaxiLatam/RadioTaxiLatamApplication.java
@@ -0,0 +1,13 @@
+package com.example.RadioTaxiLatam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioTaxiLatamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioTaxiLatamApplication.class, args);
+	}
+
+}
